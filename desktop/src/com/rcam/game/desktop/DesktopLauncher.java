9c3f3890ec94a730799e3998cc62d87230141d38
Rodolfo Cam II <rodiicam@gmail.com>
2017-04-14 13:43:15 +0800

    Initial push to master.

---
+++ desktop/src/com/rcam/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rcam.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rcam.game.TapRunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TapRunner(), config);
+	}
+}
