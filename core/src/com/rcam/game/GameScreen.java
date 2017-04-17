8ef6e18e2a30f34d4f8fa114870bf264ab25ff95
Rodolfo Cam II <rodiicam@gmail.com>
2017-04-18 02:12:09 +0800

    Refactored buttons and speed meter in Hud class.

--- core/src/com/rcam/game/GameScreen.java
+++ core/src/com/rcam/game/GameScreen.java
@@ -1,17 +1,8 @@
 package com.rcam.game;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.Array;
 
 /**
@@ -25,12 +16,7 @@ public class GameScreen implements Screen{
     Runner runner;
     Array<Ground> grounds;
     Ground grnd;
-
-    Stage stage;
-    TextButton button1, button2;
-    ProgressBar meter;
-    BitmapFont font;
-
+    Hud hud;
 
     public GameScreen(final TapRunner gam){
         this.game = gam;
@@ -44,52 +30,8 @@ public class GameScreen implements Screen{
         grounds.add(new Ground(0));
         grounds.add(new Ground(grnd.getTexture().getWidth()));
 
-        Skin mySkin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
+        hud = new Hud(runner);
 
-        stage = new Stage();
-        Gdx.input.setInputProcessor(stage);
-        font = new BitmapFont();
-        Table table = new Table();
-        table.setFillParent(true);
-//        stage.setDebugAll(true);
-
-        meter = new ProgressBar(1, 15, 1, false, mySkin, "default-horizontal");
-        meter.setAnimateDuration(.5f);
-
-        button1 = new TextButton("Run",mySkin,"arcade");
-        button1.addListener(new InputListener(){
-            @Override
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-            }
-            @Override
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                if(runner.isOnGround)
-                    runner.run();
-                return true;
-            }
-        });
-
-        button2 = new TextButton("Jump",mySkin,"arcade");
-        button2.addListener(new InputListener(){
-            @Override
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-            }
-            @Override
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                if(runner.isOnGround)
-                    runner.jump();
-                return true;
-            }
-        });
-
-        table.add(meter).size(100,100);
-        table.row();
-        table.add(button1).padLeft(20).width(100).height(100).expandX();
-        table.add(button2).padRight(20).width(100).height(100).expandX();
-        table.row();
-        table.center().bottom();
-        table.padBottom(50);
-        stage.addActor(table);
     }
 
     public void handleInput() {
@@ -115,41 +57,9 @@ public class GameScreen implements Screen{
         runner.update(delta); //render first then logic?
 
         //update meter TODO: refactor to have own class Hud.Meter
-        System.out.println(runner.getSpeed().x);
-        if(runner.getSpeed().x < 100)
-            meter.setValue(1);
-        else if(runner.getSpeed().x < 200)
-            meter.setValue(2);
-        else if(runner.getSpeed().x < 300)
-            meter.setValue(3);
-        else if(runner.getSpeed().x < 400)
-            meter.setValue(4);
-        else if(runner.getSpeed().x < 500)
-            meter.setValue(5);
-        else if(runner.getSpeed().x < 600)
-            meter.setValue(6);
-        else if(runner.getSpeed().x < 700)
-            meter.setValue(7);
-        else if(runner.getSpeed().x < 800)
-            meter.setValue(8);
-        else if(runner.getSpeed().x < 900)
-            meter.setValue(9);
-        else if(runner.getSpeed().x < 1000)
-            meter.setValue(10);
-        else if(runner.getSpeed().x < 1200)
-            meter.setValue(11);
-        else if(runner.getSpeed().x < 1400)
-            meter.setValue(12);
-        else if(runner.getSpeed().x < 1600)
-            meter.setValue(13);
-        else if(runner.getSpeed().x < 1800)
-            meter.setValue(14);
-        else if(runner.getSpeed().x < 2000)
-            meter.setValue(15);
-
+        hud.meter.update(runner.getSpeed().x);
         game.batch.end();
-        stage.act();
-        stage.draw();
+        hud.render();
     }
 
     @Override
@@ -183,7 +93,6 @@ public class GameScreen implements Screen{
         grnd.dispose();
         for(Ground ground : grounds)
             ground.dispose();
-        font.dispose();
-        stage.dispose();
+        hud.dispose();
     }
 }
---
+++ core/src/com/rcam/game/Hud.java
@@ -0,0 +1,156 @@
+package com.rcam.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+/**
+ * Created by Rod on 4/18/2017.
+ */
+
+public class Hud {
+    Stage stage;
+    Table table;
+    Skin mySkin;
+    Meter meter;
+    RunButton runButton;
+    JumpButton jumpButton;
+
+
+    public Hud(final Runner runner){
+        table = new Table();
+        table.setFillParent(true);
+        mySkin = new Skin(Gdx.files.internal("skin/clean-crispy-ui.json"));
+        stage = new Stage();
+        meter = new Meter();
+        runButton = new RunButton(runner);
+        jumpButton = new JumpButton(runner);
+
+        Gdx.input.setInputProcessor(stage);
+
+        table.add(meter.getSpeedMeter()).size(100,100);
+        table.row();
+        table.add(runButton.getRunButton()).padLeft(20).width(100).height(100).expandX();
+        table.add(jumpButton.getJumpButton()).padRight(20).width(100).height(100).expandX();
+        table.row();
+        table.center().bottom();
+        table.padBottom(50);
+        stage.addActor(table);
+    }
+
+    public class Meter{
+        ProgressBar speedMeter;
+
+        public Meter(){
+            speedMeter = new ProgressBar(1, 15, 1, false, getMySkin(), "default-horizontal");
+            speedMeter.setAnimateDuration(.5f);
+        }
+
+        public ProgressBar getSpeedMeter() {
+            return speedMeter;
+        }
+
+        public void update(float speed){
+            if(speed < 100)
+                speedMeter.setValue(1);
+            else if(speed < 200)
+                speedMeter.setValue(2);
+            else if(speed < 300)
+                speedMeter.setValue(3);
+            else if(speed < 400)
+                speedMeter.setValue(4);
+            else if(speed < 500)
+                speedMeter.setValue(5);
+            else if(speed < 600)
+                speedMeter.setValue(6);
+            else if(speed < 700)
+                speedMeter.setValue(7);
+            else if(speed < 800)
+                speedMeter.setValue(8);
+            else if(speed < 900)
+                speedMeter.setValue(9);
+            else if(speed < 1000)
+                speedMeter.setValue(10);
+            else if(speed < 1200)
+                speedMeter.setValue(11);
+            else if(speed < 1400)
+                speedMeter.setValue(12);
+            else if(speed < 1600)
+                speedMeter.setValue(13);
+            else if(speed < 1800)
+                speedMeter.setValue(14);
+            else if(speed < 2000)
+                speedMeter.setValue(15);
+        }
+    }
+
+    public class RunButton{
+        TextButton button;
+
+        public RunButton(Runner runner){
+            button = new TextButton("Run",mySkin,"arcade");
+            button1Listener(button, runner);
+        }
+
+        public void button1Listener(TextButton button, final Runner runner){
+            button.addListener(new InputListener(){
+                @Override
+                public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                    if(runner.isOnGround)
+                        runner.run();
+                    return true;
+                }
+            });
+        }
+
+        public TextButton getRunButton(){
+            return button;
+        }
+
+    }
+
+    public class JumpButton{
+        TextButton button;
+
+        public JumpButton(Runner runner){
+            button = new TextButton("Jump",mySkin,"arcade");
+            buttonListener(button, runner);
+        }
+
+        public void buttonListener(TextButton button, final Runner runner){
+            button.addListener(new InputListener(){
+                @Override
+                public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                    if(runner.isOnGround)
+                        runner.jump();
+                    return true;
+                }
+            });
+        }
+
+        public TextButton getJumpButton(){
+            return button;
+        }
+
+    }
+
+    public Skin getMySkin(){
+        return mySkin;
+    }
+
+    public void render(){
+        stage.act();
+        stage.draw();
+    }
+
+    public void dispose(){
+        stage.dispose();
+    }
+
+}
