d7dbd29c64abd953a78b0bca21a24588e60f9e43
Rodolfo Cam II <rodiicam@gmail.com>
2017-07-02 03:17:20 +0800

    Adjustments.

--- core/src/com/rcam/game/MainMenuScreen.java
+++ core/src/com/rcam/game/MainMenuScreen.java
@@ -120,6 +120,7 @@ public class MainMenuScreen implements Screen {
             @Override
             public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
                 Gdx.app.exit();
+                dispose();
                 return true;
             }
         });
